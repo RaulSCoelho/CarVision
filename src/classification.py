d4f08fdb89e78779f13642edebc0843d544b73a2
RaulSCoelho <raul.s.coelho@gmail.com>
2024-04-06 15:24:44 -0300

    classify complete

--- src/classification.py
+++ src/classification.py
@@ -1,7 +1,11 @@
+import os
+import random
+from typing import List
 import torch
 from torchvision import transforms
+from matplotlib import pyplot as plt
 from utils.data import mat_to_list
-import PIL.Image as Image
+from PIL import Image
 
 cars_model = torch.load('best_cars_model.pth')
 classes_df = mat_to_list('src/data/cars_annos.mat', 'class_names')
@@ -15,13 +19,50 @@ cars_transforms = transforms.Compose([
     transforms.Normalize(torch.Tensor(mean), torch.Tensor(std))
 ])
 
-def classify(model: torch.nn.Module, image_transforms: transforms.Compose, image_path: str, classes: list[str]):
+def classify(model: torch.nn.Module, image_transforms: transforms.Compose, image_paths: str, classes: List[str]):
     model = model.eval()
-    image = Image.open(image_path)
-    image = image_transforms(image).float()
-    image = image.unsqueeze(0)
 
-    output = model(image)
-    _, predicted = torch.max(output.data, 1)
+    if isinstance(image_paths, str):
+        image_paths = [image_paths]
 
-    print(f"Predicted class: {predicted.item()}")
+    images = {image_path: Image.open(image_path) for image_path in image_paths}
+
+    num_images = len(images)
+    cols = min(3, num_images)  # Limit to at most 3 columns
+    rows = (num_images + cols - 1) // cols  # Calculate required rows
+
+    total_width = sum(image.width for image in images.values())
+    total_height = max(image.height for image in images.values())
+
+    _, axes = plt.subplots(rows, cols, figsize=(total_width // 100, total_height // 100))
+
+    for i, (image_path, image) in enumerate(images.items()):
+        total_width += image.width
+        total_height += image.height
+        image_transformed = image_transforms(image).float()
+        image_transformed = image_transformed.unsqueeze(0)
+
+        output = model(image_transformed)
+        _, predicted = torch.max(output.data, 1)
+
+        predicted_class = classes[predicted.item()]
+        print(f"{image_path}: {predicted_class}")
+
+        if num_images == 1:
+            axes.imshow(image)  # Use flattened axes for efficient indexing
+            axes.set_title(predicted_class)
+            axes.axis('off')
+        else:
+            axes.flat[i].imshow(image)  # Use flattened axes for efficient indexing
+            axes.flat[i].set_title(predicted_class)
+            axes.flat[i].axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
+# Get a random image path from the cars_test directory
+cars_test_dir = 'src/data/cars_test'
+image_dirs = os.listdir(cars_test_dir)
+image_paths = [os.path.join(cars_test_dir, dir) for dir in random.sample(image_dirs, 6)]
+
+classify(cars_model, cars_transforms, image_paths, classes_df)
